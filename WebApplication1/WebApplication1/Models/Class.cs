29942cc60e94014d3f891f4389ce8a8b6c00e7df
blank <jkookship@gmail.com>
2024-11-11 14:07:09 +0200

    Backup

---
+++ WebApplication1/WebApplication1/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    //make migration ecery time u change the MODEL. not controller or view.
+    //after migration you have to update database
+    // if u go into package viewer.. idk? u should see stuff
+    public class Class
+    {
+        public int Id { get; set; }
+        public string AccountTypeName { get; set; }
+    }
+}
